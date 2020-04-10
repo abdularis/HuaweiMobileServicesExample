cf921dd61a94bf01590cd59df45519bb7a09bc96
Abdul Aris <abdularisrahmanudin10@gmail.com>
2020-04-10 17:58:39 +0700

    add menu screen

--- app/src/main/java/com/aar/tryhuawei/MainActivity.kt
+++ app/src/main/java/com/aar/tryhuawei/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.aar.tryhuawei
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAccountKit.setOnClickListener {
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        }
     }
 }
